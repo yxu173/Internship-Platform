6f48f69ae1ec2d1bce3af368ce459dc30f3f6173
yxu <mohamedsamir177@outlook.com>
2025-01-30 18:15:58 +0200

    Add Project Structure and User Identity

---
+++ src/Domain/Users/User.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Users;
+
+public sealed class User : IdentityUser<Guid>
+{
+    private User()
+    {
+    }
+
+    public string FullName { get; private set; }
+    public static User Create(string email, string userName)
+    {
+        return new User
+        {
+            Id = Guid.NewGuid(),
+            Email = email,
+            UserName = userName
+        };
+    }
+}
