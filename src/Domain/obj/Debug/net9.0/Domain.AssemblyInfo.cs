6ea0e46fbce985c0c668913a3f1b85ddea277b1e
yxu <mohamedsamir177@outlook.com>
2025-04-09 22:34:46 +0000

    Update project dependencies and connection strings for PostgreSQL

--- src/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a139a5919f8aca3854403455b40c1e83a1f477f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71fb8af28d9ce3cf0f6e95ea2b14587bb8b93779")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
