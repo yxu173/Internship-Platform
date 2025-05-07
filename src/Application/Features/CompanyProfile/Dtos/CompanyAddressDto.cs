d19e67a6e2f23e9bf234305af9cb5e48fd324fbd
yxu <mohamedsamir177@outlook.com>
2025-05-07 19:31:54 +0300

    Add email to CompanyAddressDto and query handler logic
    Introduced the `Email` field to `CompanyAddressDto` and updated `GetCompleteCompanyProfileQueryHandler` to fetch the user's email from `IUserRepository`. This change ensures the email is included when returning the complete company profile.

--- src/Application/Features/CompanyProfile/Dtos/CompanyAddressDto.cs
+++ src/Application/Features/CompanyProfile/Dtos/CompanyAddressDto.cs
@@ -1,6 +1,7 @@
 namespace Application.Features.CompanyProfile.Dtos;
 
 public sealed record CompanyAddressDto(
+    string Email,
     string Governorate,
     string City,
     string Street
--- src/Application/Features/CompanyProfile/Queries/GetCompleteCompanyProfile/GetCompleteCompanyProfileQueryHandler.cs
+++ src/Application/Features/CompanyProfile/Queries/GetCompleteCompanyProfile/GetCompleteCompanyProfileQueryHandler.cs
@@ -10,15 +10,18 @@ public sealed class
     GetCompleteCompanyProfileQueryHandler : IQueryHandler<GetCompleteCompanyProfileQuery, CompleteCompanyProfile>
 {
     private readonly ICompanyRepository _companyRepository;
+    private readonly IUserRepository _userRepository;
 
-    public GetCompleteCompanyProfileQueryHandler(ICompanyRepository companyRepository)
+    public GetCompleteCompanyProfileQueryHandler(ICompanyRepository companyRepository, IUserRepository userRepository)
     {
         _companyRepository = companyRepository;
+        _userRepository = userRepository;
     }
 
     public async Task<Result<CompleteCompanyProfile>> Handle(GetCompleteCompanyProfileQuery request,
         CancellationToken cancellationToken)
     {
+        var user = await _userRepository.GetByIdAsync(request.Id);
         var company = await _companyRepository.GetByCompanyProfileWithInternships(request.Id,
             p => new CompleteCompanyProfile(
                 p.LogoUrl,
@@ -29,6 +32,7 @@ public sealed class
                     p.Size.ToString(),
                     p.WebsiteUrl),
                 new CompanyAddressDto(
+                    user.Email,
                     p.Address.Governorate.ToString(),
                     p.Address.City,
                     p.Address.Street
